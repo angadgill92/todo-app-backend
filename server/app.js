c69c74e7f9cebdaac7606e9b7fe63aad5ad6f151
Angad Gill <angadpalsingh92@gmail.com>
2016-10-03 14:55:52 +0530

    Changed the comment format on server side

--- server/app.js
+++ server/app.js
@@ -48,8 +48,8 @@ io.on('connection', function (socket) {
   })
 
   socket.on('sendmessage', function (data) {
-    var assignName = data.to
-    users[assignName].emit('discuss', { msg: data.msg, name: socket.name })
+    var recipient = data.to
+    users[recipient].emit('incoming', { "sentBy": data.sentBy, "time": data.time "message": data.message })
   })
 })
 
